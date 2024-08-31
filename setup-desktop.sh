3a158091dd0ed25450d0012c37a006a67214eb44
beech <beechmanbopp@gmail.com>
2024-08-31 18:02:47 -0500

    Upload

---
+++ setup-desktop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Placeholder
+
+echo "-------------------------------------------------"
+echo "Setting Up Desktop - Installing Sudo"
+echo "-------------------------------------------------"
+
+#  Install sudo
+apt update
+apt install sudo -yy
+
+#  Find the standard user you created during installation and make it a variable
+user=$(getent passwd 1000 |  awk -F: '{ print $1}')
+
+#  Echo the user into the sudoers file
+echo "$user  ALL=(ALL:ALL)  ALL" >> /etc/sudoers
+
