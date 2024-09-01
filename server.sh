469e4795adf6699094ddd5543df0ebf052df7910
beech <beechmanbopp@gmail.com>
2024-09-01 14:11:17 -0500

    Server testing

--- server.sh
+++ server.sh
@@ -13,14 +13,14 @@ select opt in "${choice[@]}"; do
             ;;
         "Install Docker")
             echo "Installing Docker"
-            curl -fsSL https://get.docker.com -o get-docker.sh
-            sudo sh get-docker.sh
-            COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-            sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-            sudo chmod +x /usr/local/bin/docker-compose
-            sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
-            user=$(getent passwd 1000 |  awk -F: '{ print $1}')
-            sudo usermod -aG docker $USER
+            curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null
+            sudo sh get-docker.sh > /dev/null
+            COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4) > /dev/null
+            sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose > /dev/null
+            sudo chmod +x /usr/local/bin/docker-compose > /dev/null
+            sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose > /dev/null
+            user=$(getent passwd 1000 |  awk -F: '{ print $1}') > /dev/null
+            sudo usermod -aG docker $USER > /dev/null
             echo "Docker setup complete. Please log out & log back in after exiting to use without sudo."
             ;;
 	"Exit")
