5cac51c43ab5b402967246e7459ac1838373f90f
beech <beechmanbopp@gmail.com>
2024-09-01 20:15:03 -0500

    Updated server test

--- server.sh
+++ server.sh
@@ -4,7 +4,7 @@ choice=("Setup Sudo" "Install Docker" "Exit")
 select opt in "${choice[@]}"; do
     case $opt in
         "Setup Sudo")
-            echo "Setting up sudo"
+            echo "Setting up sudo..."
             apt update > /dev/null 2>&1
             apt install sudo -yy > /dev/null 2>&1
             user=$(getent passwd 1000 |  awk -F: '{ print $1}') > /dev/null 2>&1
@@ -13,12 +13,14 @@ select opt in "${choice[@]}"; do
             echo "Sudo setup complete."
             ;;
         "Install Docker")
-            echo "Installing Docker"
+            echo "Installing Docker..."
             curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null 2>&1
+            echo "..."
             sudo sh get-docker.sh > /dev/null 2>&1
             COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4) > /dev/null 2>&1
             sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose > /dev/null 2>&1
             sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
+            echo "..."
             sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose > /dev/null 2>&1
             user=$(getent passwd 1000 |  awk -F: '{ print $1}') > /dev/null 2>&1
             sudo usermod -aG docker $USER > /dev/null 2>&1
